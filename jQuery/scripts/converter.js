b4c670ebcb75d5af719c98f49d070e91aa0ac50d
Bulzay <goawaylol@yandex.ru>
2023-01-17 01:00:05 +0300

    main logic funcs added

---
+++ jQuery/scripts/converter.js
@@ -0,0 +1,25 @@
+import { dict, dictUpper } from dictionaries;
+
+
+function innerSwap (letter, array, shift) {
+    if ((array.indexOf(letter) + shift) <= array.length) {
+        return (array[(array.indexOf(letter) + shift)]);
+    } else {
+        return (array[(array.indexOf(letter) + shift) - 33]);
+    }
+}
+
+
+function convert (text, iter) {
+    let result = [];
+    for (let i of text) {
+        if (dict.includes(i)) {
+            result.push(innerSwap(i, dict, iter));
+        } else if (dictUpper.includes(i)) {
+            result.push(innerSwap(i, dictUpper, iter));
+        } else {
+            result.push(i);
+        }
+    }
+    return result.join('');
+}
---
+++ jQuery/scripts/deconverter.js
@@ -0,0 +1,25 @@
+import { dict, dictUpper } from dictionaries;
+
+
+function deconvSwap (letter, array, shift) {
+    if ((array.indexOf(letter) - shift) >= 0) {
+        return (array[(array.indexOf(letter) - shift)]);
+    } else {
+        return (array[(array.indexOf(letter) - shift) + 33]);
+    }
+}
+
+
+function deconvert (text, iter) {
+    let result = [];
+    for (let i of text) {
+        if (dict.includes(i)) {
+            result.push(deconvSwap(i, dict, iter));
+        } else if (dictUpper.includes(i)) {
+            result.push(deconvSwap(i, dictUpper, iter));       
+        } else {
+            result.push(i);
+        }
+    }
+    return result.join('');
+}
